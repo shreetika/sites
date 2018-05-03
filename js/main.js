8098b7b8ccf42a078567798346006ad4008a45e0
shreetika <bistashreetika04@gmail.com>
2018-05-03 21:56:01 +0545

    new commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function check(){
+	var name= document.webpage.name.value;
+	var password= document.webpage.password.value;
+	 if (name!='' && password !=''){
+	 	document.getElementById("after_submit").style.visibility="visible";
+	 	// console.log('lol');
+		var button = alert("WELCOME");
+	}
+	
+
+    
+    
+ }
---
+++ js/second.js
@@ -0,0 +1,15 @@
+function check(){
+	var name= document.webpage.name.value;
+	var email= document.webpage.email.value;
+	 if (name!='' && email !=''){
+	 	document.getElementById("after_submit").style.visibility="visible";
+	 	// console.log('lol');
+		var button = prompt("Please enter your message");
+
+	}
+	
+
+    
+    
+ }
+ 
